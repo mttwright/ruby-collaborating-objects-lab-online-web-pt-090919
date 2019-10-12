3a85365fc69c182430a7701ad68dbcc0f7d473d6
virtual-frame-7789 <mttwright@gmail.com>
2019-10-12 14:44:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,11 @@ class Song
   end
   
   def artist_name=(name)
-    self.artist.name = name
+    if self.artist
+      self.artist.name = name
+    else
+      nil
+    end
   end
   
   def self.new_by_filename(filename)
